977fbe04686126974ca7011092df8867d9d4cade
Maximlo <maximl@moveo.co.il>
2021-11-02 14:34:26 +0200

    initial commit

---
+++ Petish-iOS/Petish-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Petish-iOS
+//
+//  Created by Maxim Lobovich on 02/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
