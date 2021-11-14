eba6df3a23c49813aa88a5352b638a5fe8018613
Maximlo <maximl@moveo.co.il>
2021-11-14 16:21:22 +0200

    completed constraints for signup page added segues between screens temporary login and home pages lacking checkbox

--- Petish-iOS/Petish-iOS/Controller/SignUpViewController.swift
+++ Petish-iOS/Petish-iOS/Controller/SignUpViewController.swift
@@ -3,10 +3,21 @@ import UIKit
 class SignUpViewController: UIViewController {
 
     
-    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var nameField: BottomBorderTextField!
+    @IBOutlet weak var emailField: BottomBorderTextField!
+    @IBOutlet weak var passwordField: BottomBorderTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    
+    @IBAction func signupPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func withFacebookPressed(_ sender: UIButton) {
+    }
+    
+    
 }
 
