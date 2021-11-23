f6a4029605b28f40a78ee37181f6191c471c699c
Maximlo <maximl@moveo.co.il>
2021-11-23 18:16:00 +0200

    overlay popup finished

--- Petish-iOS/Petish-iOS/Controller/CreatePetViewController.swift
+++ Petish-iOS/Petish-iOS/Controller/CreatePetViewController.swift
@@ -2,22 +2,38 @@ import UIKit
 
 class CreatePetViewController: BaseViewController {
 
-    let popUpView = AddPictureOverlayViewController()
-    
     @IBOutlet weak var petUIView: UIView!
     @IBOutlet weak var petImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configurePetUIView()
+        viewInitialSettings()
     }
     
-    func configurePetUIView(){ // to be changed
+    func viewInitialSettings(){ // to be changed
         petUIView.layer.cornerRadius = 60
+        petImageView.layer.cornerRadius = 60
+        petImageView.contentMode = .center
+        petImageView.image = #imageLiteral(resourceName: "AddCross")
     }
     
     @IBAction func addPhotoPressed(_ sender: UIButton) {
+        let popUpView = AddPictureOverlayViewController()
+        popUpView.delegate = self
         displayView(displayView: popUpView)
+    }
+}
+
+extension CreatePetViewController: AddPictureOverlayDelegate{
+    func resetImage() {
+        viewInitialSettings()
+    }
+    
+    func setImage(image: UIImage) {
+        petImageView.image = image
+        descriptionLabel.text = "EDIT PHOTO"
+        petImageView.contentMode = .scaleAspectFill
     }
 }
--- Petish-iOS/Petish-iOS/View/addPictureOverlay/AddPictureOverlayViewController.swift
+++ Petish-iOS/Petish-iOS/View/addPictureOverlay/AddPictureOverlayViewController.swift
@@ -1,25 +1,57 @@
 import UIKit
 
-class AddPictureOverlayViewController: BaseViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+protocol AddPictureOverlayDelegate{
+    func setImage(image: UIImage)
+    func resetImage()
+}
 
+class AddPictureOverlayViewController: BaseViewController {
+
+    private var imageToSet: UIImage?
+    var delegate: AddPictureOverlayDelegate?
+
+    @IBOutlet weak var uploadButton: FilledPurpleButton!
+    
     let imagePickerController = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        uploadButton.disable()
         imagePickerController.delegate = self
         imagePickerController.sourceType = .photoLibrary
-
     }
-    
     
     @IBAction func closeOverlay(_ sender: UIButton) {
         dismissView()
     }
     
-    @IBAction func takePicturePressed(_ sender: UIButton) {
-    }
-    
     @IBAction func fromGalleryPressed(_ sender: UIButton) {
         self.present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func resetPressed(_ sender: TransparentPurpleButton) {
+        delegate?.resetImage()
+        dismissView()
+    }
+    
+    @IBAction func uploadPressed(_ sender: FilledPurpleButton) {
+        if let image = imageToSet{
+            delegate?.setImage(image: image)
+        }
+        dismissView()
+    }
+}
+
+extension AddPictureOverlayViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print("\(info)") // debug
+        imageToSet = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+        uploadButton.enable()
     }
 }
